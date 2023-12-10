1253ed04773cc2fd7ddd342be74c12df72c0a080
gani.lala3 <gani.lala3@gmail.com>
2023-12-10 14:29:26 +0100

    Finalizimi i Fazes se Pare

--- Honda Ks/Home/home.js
+++ Honda Ks/Home/home.js
@@ -1,17 +1,18 @@
+const imageElement = document.getElementById("imageSlider");
+
 const imageUrls = [
-    "../asetet/first.jpg",
-    "../asetet/atv.avif",
-    "../asetet/gene3.jpg",
-    "../asetet/atv2.jpg",
-  ];
-  
-  let currentIndex = 0;
-  
-  function changeImage() {
-    const imageElement = document.getElementById("imageSlider");
-    imageElement.src = imageUrls[currentIndex];
-  
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-  }
-  
-  setInterval(changeImage, 3500);
+  "../asetet/first.jpg",
+  "../asetet/atv.avif",
+  "../asetet/gene3.jpg",
+  "../asetet/atv2.jpg",
+];
+
+let currentIndex = 0;
+
+function changeImage() {
+  imageElement.src = imageUrls[currentIndex];
+
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+}
+
+setInterval(changeImage, 3500);
--- Honda Ks/Home/menu.js
+++ Honda Ks/Home/menu.js
@@ -1,7 +1,42 @@
-const toggleMenu = document.getElementById("menu")
-const menuVisible = document.getElementById("menu-visible")
+const toggleMenu = document.getElementById("menu");
+const menuVisible = document.getElementById("menu-visible");
+const closeMenu = document.getElementById("close-menu");
+const MOBILE_UP = document.getElementById("mobile-up");
+const bgMargin = document.getElementById("margin-top");
 
 const toggle = () => {
-    toggleMenu.classList.toggle("visible");
-  };
-  menuVisible.addEventListener("click",toggle)
+  toggleMenu.classList.toggle("visible");
+};
+menuVisible.addEventListener("click", toggle);
+const closeMenuMain = () => {
+  toggleMenu.classList.remove("visible");
+};
+closeMenu.addEventListener("touchmove", closeMenuMain);
+closeMenu.addEventListener("click", closeMenuMain);
+
+const mobileUp = () => {
+  MOBILE_UP.classList.add("up");
+  bgMargin.classList.add("mg-t");
+};
+
+const mobileDown = () => {
+  MOBILE_UP.classList.remove("up");
+  bgMargin.classList.remove("mg-t");
+};
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  const currentScroll =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    mobileDown();
+  } else {
+    mobileUp();
+  }
+  if (currentScroll <= 0) {
+    mobileDown();
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
